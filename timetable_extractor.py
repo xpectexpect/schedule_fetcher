a46521ceaa2b2fbe74bdb8476439f90c20e39df5
xpectexpect <ante.topos.m@gmail.com>
2026-01-19 10:50:37 +0100

    Refactor timetable_extractor.py for clarity and structure

--- timetable_extractor.py
+++ timetable_extractor.py
@@ -261,10 +261,11 @@ if __name__ == "__main__":
     pdf_link = timetable_fetcher.fetch_timetable()
 
     base_path = os.path.dirname(os.path.abspath(__file__))
-    saved_schedules_path = pathlib.path(os.path.join(base_path, "saved_schedules"))
+    saved_schedules_path = pathlib.Path(os.path.join(base_path, "saved_schedules"))
     saved_schedules_path.mkdir(parents=True, exist_ok=True)
 
     save_whole_schedule_data(pdf_link, saved_schedules_path)
+
 
 
 
